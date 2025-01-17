ae0606f9d1f682eb9700b9109e9355bdaba9499c
mohamed202323 <mohamedsaad202121ok@gmail.com>
2025-01-17 22:16:18 +0200

    configure fire base init

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
-
+import 'dart:io';
 import 'feroo_store_app.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options:
+    const FirebaseOptions(
+        apiKey: "AIzaSyA9a-bjuxLRAGvdmPCOT7Qg8t3O3-raqOs",
+        appId:"1:587995618710:android:9a93bdb2f1ac79192251b7",
+        messagingSenderId:"587995618710",
+        projectId: "feroo-store"
+    )
+  );
   runApp(const MyApp());
 }
 
