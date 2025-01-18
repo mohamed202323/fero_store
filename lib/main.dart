699942d5ce769b3da8abd0dca578ffd9ee92b58e
mohamed202323 <mohamedsaad202121ok@gmail.com>
2025-01-18 17:08:07 +0200

    add env

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
+import 'package:feroo_store/core/app/env.variabels.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'feroo_store_app.dart';
 void main()async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EnvVariabels.instance.init(envType: envTypeEnum.dev);
   await Firebase.initializeApp(
       options:
       const FirebaseOptions(
           apiKey: 'AIzaSyA9a-bjuxLRAGvdmPCOT7Qg8t3O3-raqOs',
           appId:'1:587995618710:android:9a93bdb2f1ac79192251b7',
           messagingSenderId:'587995618710',
-          projectId: 'feroo-store'
-      )
+          projectId: 'feroo-store',
+      ),
   );
-  runApp(const MyApp());
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,DeviceOrientation.portraitUp,
+  ]).then((_){
+    runApp(const MyApp());
+  });
 }
 
 
