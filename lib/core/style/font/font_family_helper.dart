e5d91f9c1be7d968f39f5a45f3f6cdef7e06ae39
mohamed202323 <mohamedsaad202121ok@gmail.com>
2025-01-19 14:24:17 +0200

    add font family

---
+++ lib/core/style/font/font_family_helper.dart
@@ -0,0 +1,7 @@
+
+class FontFamilyHelper{
+
+  static const String arabic_cairo = 'cairo' ;
+  static const String english_poppins = 'poppins' ;
+
+}
--- lib/feroo_store_app.dart
+++ lib/feroo_store_app.dart
@@ -1,7 +1,10 @@
 import 'package:feroo_store/core/app/connectivity_network_controller.dart';
 import 'package:feroo_store/core/app/env.variabels.dart';
 import 'package:feroo_store/core/common/screan/no_network_screan.dart';
+import 'package:feroo_store/core/style/font/font_size.dart';
 import 'package:flutter/material.dart';
+
+import 'core/style/font/font_family_helper.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,8 +18,25 @@ class MyApp extends StatelessWidget {
           return MaterialApp(
             debugShowCheckedModeBanner: EnvVariabels.instance.debugMode,
             title: 'Feroo_Store',
-             home: const Scaffold(
-               body: Text("moo"),
+             home: Scaffold(
+               body:Column(
+                 children: [
+
+                   const Text("==================no change======================"),
+                   const Text("mohamed"),
+                   const Text("محمد"),
+                   const Text("====================large=================="),
+                   Text("saad",style: TextStyle(fontFamily: FontFamilyHelper.english_poppins,fontWeight: FontSize.large),),
+                   Text("سعد",style: TextStyle(fontFamily: FontFamilyHelper.arabic_cairo,fontWeight: FontSize.large)),
+                   const Text("===================medium======================"),
+                   Text("ahmed",style: TextStyle(fontFamily: FontFamilyHelper.english_poppins,fontWeight: FontSize.medium),),
+                   Text("احمد",style: TextStyle(fontFamily: FontFamilyHelper.arabic_cairo,fontWeight: FontSize.medium)),
+                   const Text("======================small==================="),
+                   Text("behary",style: TextStyle(fontFamily: FontFamilyHelper.english_poppins,fontWeight: FontSize.small),),
+                   Text("بحيري",style: TextStyle(fontFamily: FontFamilyHelper.arabic_cairo,fontWeight: FontSize.small)),
+                   const Text("========================================="),
+                 ],
+               )
              ),
             builder: (context,widget){
               ConnectivityNetworkController.instance.init();
