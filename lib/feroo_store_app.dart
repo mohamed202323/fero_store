a7afc0ffb39432615491f1e36daac612acd7891c
mohamed202323 <mohamedsaad202121ok@gmail.com>
2025-01-19 18:14:43 +0200

    add screan util init

--- lib/feroo_store_app.dart
+++ lib/feroo_store_app.dart
@@ -3,6 +3,7 @@ import 'package:feroo_store/core/app/env.variabels.dart';
 import 'package:feroo_store/core/common/screan/no_network_screan.dart';
 import 'package:feroo_store/core/style/font/font_size.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/style/font/font_family_helper.dart';
 
@@ -15,39 +16,43 @@ class MyApp extends StatelessWidget {
         valueListenable: ConnectivityNetworkController.instance.isConnect,
         builder: (_,value,__){
           if(value){
-          return MaterialApp(
-            debugShowCheckedModeBanner: EnvVariabels.instance.debugMode,
-            title: 'Feroo_Store',
-             home: Scaffold(
-               body:Column(
-                 children: [
+          return ScreenUtilInit(
+            designSize: const Size(360, 690),
+            minTextAdapt: true,
+            child: MaterialApp(
+              debugShowCheckedModeBanner: EnvVariabels.instance.debugMode,
+              title: 'Feroo_Store',
+               home: Scaffold(
+                 body:Column(
+                   children: [
 
-                   const Text("==================no change======================"),
-                   const Text("mohamed"),
-                   const Text("محمد"),
-                   const Text("====================large=================="),
-                   Text("saad",style: TextStyle(fontFamily: FontFamilyHelper.english_poppins,fontWeight: FontSize.large),),
-                   Text("سعد",style: TextStyle(fontFamily: FontFamilyHelper.arabic_cairo,fontWeight: FontSize.large)),
-                   const Text("===================medium======================"),
-                   Text("ahmed",style: TextStyle(fontFamily: FontFamilyHelper.english_poppins,fontWeight: FontSize.medium),),
-                   Text("احمد",style: TextStyle(fontFamily: FontFamilyHelper.arabic_cairo,fontWeight: FontSize.medium)),
-                   const Text("======================small==================="),
-                   Text("behary",style: TextStyle(fontFamily: FontFamilyHelper.english_poppins,fontWeight: FontSize.small),),
-                   Text("بحيري",style: TextStyle(fontFamily: FontFamilyHelper.arabic_cairo,fontWeight: FontSize.small)),
-                   const Text("========================================="),
-                 ],
-               )
-             ),
-            builder: (context,widget){
-              ConnectivityNetworkController.instance.init();
-              return Scaffold(
-                body: Builder(
-                    builder:(context){
-                      return widget! ;
-                    },
-                ),
-              );
-            },
+                     const Text("==================no change======================"),
+                     const Text("mohamed"),
+                     const Text("محمد"),
+                     const Text("====================large=================="),
+                     Text("saad",style: TextStyle(fontFamily: FontFamilyHelper.english_poppins,fontWeight: FontSize.large),),
+                     Text("سعد",style: TextStyle(fontFamily: FontFamilyHelper.arabic_cairo,fontWeight: FontSize.large)),
+                     const Text("===================medium======================"),
+                     Text("ahmed",style: TextStyle(fontFamily: FontFamilyHelper.english_poppins,fontWeight: FontSize.medium),),
+                     Text("احمد",style: TextStyle(fontFamily: FontFamilyHelper.arabic_cairo,fontWeight: FontSize.medium)),
+                     const Text("======================small==================="),
+                     Text("behary",style: TextStyle(fontFamily: FontFamilyHelper.english_poppins,fontWeight: FontSize.small),),
+                     Text("بحيري",style: TextStyle(fontFamily: FontFamilyHelper.arabic_cairo,fontWeight: FontSize.small)),
+                     const Text("========================================="),
+                   ],
+                 )
+               ),
+              builder: (context,widget){
+                ConnectivityNetworkController.instance.init();
+                return Scaffold(
+                  body: Builder(
+                      builder:(context){
+                        return widget! ;
+                      },
+                  ),
+                );
+              },
+            ),
           );
           }
           else {
